e7f0db628bc6a37f94c4e9e9b4280d3d8411f773
Vestry <vestry.odessa@gmail.com>
2021-12-05 22:06:21 +0300

    feat: add basic structure

---
+++ src/common/utils.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const catchErrors = fn => async (req, res, next) => {
+  try {
+    return await fn(req, res, next);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+class CustomError extends Error {
+  constructor({ status, message }) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+}
+
+const writeToFile = (filename, data) => {
+  const pathToFile = path.resolve(filename);
+  fs.writeFileSync(pathToFile, JSON.stringify(data), 'utf-8', error => {
+    if (error) {
+      console.error(`Error caught, reason: ${error}`);
+    }
+  })
+};
+
+module.exports = {
+  catchErrors,
+  CustomError,
+  writeToFile,
+};
